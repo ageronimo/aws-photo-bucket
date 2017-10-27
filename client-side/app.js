f94eec52ed9d34a91ddd610fb8886618894c455c
ageronimo <angelageroniimo@gmail.com>
2017-10-27 10:46:03 -1000

    in progress adding accss control header

--- client-side/app.js
+++ client-side/app.js
@@ -1,1 +1,3 @@
-console.log("jkdsafj sdlfjasdlk;fjsdlf");
+console.log("jkdsafj sdlfjasdlk;fjsdlf")
+
+// link to request: http://photo-bucket-tmp-prjct.s3-us-west-2.amazon.aws.com/
--- handler.js
+++ handler.js
@@ -1,6 +1,21 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.request = (event, context, callback) => {
+  const AWS = require('aws-sdk');
+  const s3 = new AWS.S3();
+  const params = { Bucket: 'photo-bucket-tmp-prjct'};
+
+  s3.listObjects(params, function(err,data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      console.log(data)
+    }
+  })
+
+  // setting a CORS header:
+  // requestVar.setRequestHeader('Access-Control-Alow-Origin', '*')
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
